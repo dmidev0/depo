6e8fd1376475dfcd5492f45a6e8ab9c7c54430b4
Dmitry Donskoy <dmidev@yandex.ru>
2019-03-27 13:21:49 +0500

    path to dist

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 3001;
-app.use(express.static(path.join(__dirname, '/dist/wiley')));
+app.use(express.static(path.join(__dirname, '/dist/depo')));
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/dist/wiley/index.html');
+    res.sendFile(__dirname + '/dist/depo/index.html');
 });
 app.listen(port);
