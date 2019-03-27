4e2eeb3401617d65bd83c45b1bba2924554625df
Dmitry Donskoy <dmidev@yandex.ru>
2019-03-27 14:09:52 +0500

    any route

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, '/dist/depo')));
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
     res.sendFile(__dirname + '/dist/depo/index.html');
 });
 app.listen(port);
